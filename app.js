b9d3ce6e5786bdd8fe89a46c28324cf1c407f4d3
jokerxj <jokerxj@nate.com>
2017-01-12 15:10:57 +0900

    new1

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.get('/',function (req,res) {
-  res.send('hello world!');
-});
-
+app.use(express.static(path.join(__dirname+'/public')));
+console.log(__dirname);
 app.listen(3000,function(){
   console.log('server On!');
 });
